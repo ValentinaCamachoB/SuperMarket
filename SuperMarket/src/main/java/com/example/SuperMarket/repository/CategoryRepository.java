ee4c0c54643de22b17d858669c476227ff64906a
jefzt <jefrylv10@gmail.com>
2026-05-28 10:54:12 -0500

    Revert "Revert "Revert "feat(category): crear repositorio de categorías"""

--- SuperMarket/src/main/java/com/example/SuperMarket/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SuperMarket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.SuperMarket.entity.Category;
- 
-@Repository
-public interface Categoryrepository extends JpaRepository<Category, Long> {
-}
